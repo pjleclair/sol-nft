d3dbe9ff0f1e762543468b8e9cf18f7d36476b50
lompdoctor <pleclair@protonmail.com>
2023-08-24 11:42:29 -0500

    Move components into marketplace dir

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 
-import Sidebar from "../components/sidebar/sidebar"
-import Header from "../components/header/header"
-import Dashboard from "../components/dashboard/dashboard"
-import Bid from "../components/bid/bid"
-import Collection from "../components/collection/collection"
-import Profile from "../components/profile/profile"
-import Settings from "../components/settings/settings"
+import Sidebar from "./components/sidebar/sidebar"
+import Header from "./components/header/header"
+import Dashboard from "./components/dashboard/dashboard"
+import Bid from "./components/bid/bid"
+import Collection from "./components/collection/collection"
+import Profile from "./components/profile/profile"
+import Settings from "./components/settings/settings"
 
 
 import React, { ReactElement } from 'react';
